a9be98057b6ef0bb60d2bd3be7af544210f91b64
HamzaIftikhar0012 <hamzaiftikhar0012@gmail.com>
2024-06-10 20:24:00 +0500

    Updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'username'
     ];
 
+    protected $guarded = [];
+
     /**
      * The attributes that should be hidden for serialization.
      *
