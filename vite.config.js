81f0cb3a07442ba61535b904fbc67c322ef54e69
Awais7899 <hafiz.awais.8834@gmail.com>
2024-08-13 20:59:15 +0500

    build the assets

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/js/userForm.js",
-                "resources/js/admin/main.js",
-            ],
+            input: ["resources/js/userForm.js", "resources/js/admin/main.js"],
             refresh: true,
         }),
     ],
